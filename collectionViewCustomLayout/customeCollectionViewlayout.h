910da5e25fdf08365c5d0f2b1a185659b7c13b97
wanghui <2396611405@qq.com>
2016-01-23 17:20:20 +0800

    aaa

--- collectionViewCustomLayout/customeCollectionViewlayout.h
+++ collectionViewCustomLayout/customeCollectionViewlayout.h
@@ -9,10 +9,42 @@
 #import <UIKit/UIKit.h>
 @class  customeCollectionViewlayout;
 @protocol CustomecollectionViewLayoutDelegate <NSObject>
+/**
+ * 确定布局行数的回调
+ */
+@required
+- (NSInteger)numberOfColumnWithCollectionView:(UICollectionView *)collectionView
+                         collectionViewLayout:( customeCollectionViewlayout *)collectionViewLayout;
+
+/**
+ * 确定cell的Margin
+ */
+@required
+- (CGFloat)marginOfCellWithCollectionView:(UICollectionView *)collectionView
+                     collectionViewLayout:( customeCollectionViewlayout *)collectionViewLayout;
+/**
+ * 确定cell的最小高度
+ */
+@required
+- (CGFloat)minHeightOfCellWithCollectionView:(UICollectionView *)collectionView
+                        collectionViewLayout:( customeCollectionViewlayout *)collectionViewLayout;
+
+/**
+ * 确定cell的最大高度
+ */
+@required
+- (CGFloat)maxHeightOfCellWithCollectionView:(UICollectionView *)collectionView
+                        collectionViewLayout:( customeCollectionViewlayout *)collectionViewLayout;
 
 
 @end
 
+
+
+
 @interface customeCollectionViewlayout : UICollectionViewLayout
+
+@property (weak, nonatomic)id<CustomecollectionViewLayoutDelegate>layoutDelegate;
+
 
 @end
--- collectionViewCustomLayout/customeCollectionViewlayout.m
+++ collectionViewCustomLayout/customeCollectionViewlayout.m
@@ -7,6 +7,7 @@
 //
 
 #import "customeCollectionViewlayout.h"
+#define SCR
 
 @implementation customeCollectionViewlayout
 
